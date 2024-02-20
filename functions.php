8475b7943521f0cd228381cb544fc1466a443698
Aman Jamwal <aman.jamwal95@gmail.com>
2024-02-20 20:50:04 +1300

    Add footer navigation

--- functions.php
+++ functions.php
@@ -5,3 +5,25 @@ add_action('wp_enqueue_scripts', 'alh_enqueue_styles');
 function alh_enqueue_styles() {
 	wp_enqueue_style('alh-style',get_stylesheet_uri());
 }
+
+function asl_footer_menu() {
+	register_nav_menu('footer-menu',__( 'ASL Footer Menu' ));
+}
+add_action( 'init', 'asl_footer_menu' );
+
+class Custom_ASL_Nav_Menu extends Walker_Nav_Menu {
+	function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
+        $output .= "<li>";
+        if ( ! empty( $item->classes ) && in_array( 'menu-item-has-children', $item->classes ) ) {
+            // Menu item has children, do not output a link
+            $output .= $item->title;
+        } else {
+            // Menu item doesn't have children, output a link
+            $output .= "<a href='" . $item->url . "'>" . $item->title . "</a>";
+        }
+    }
+
+    function end_el( &$output, $item, $depth = 0, $args = null ) {
+        $output .= "</li>";
+    }
+}
--- patterns/footer.php
+++ patterns/footer.php
@@ -6,21 +6,31 @@
  */
 ?>
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"asl-purple","textColor":"white","layout":{"type":"constrained","justifyContent":"left"}} -->
-<div class="wp-block-group has-white-color has-asl-purple-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":null,"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
-<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"33%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33%"><!-- wp:paragraph {"fontSize":"medium"} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"asl-purple","textColor":"white","layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group has-white-color has-asl-purple-background-color has-text-color has-background has-link-color" style="margin-top:var(--wp--preset--spacing--50);padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":"center","style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"20%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20%"><!-- wp:paragraph {"fontSize":"medium"} -->
 <p class="has-medium-font-size">Aspiring Log Homes (ALH) Builds houses to the highest quality using the best quality trees from certified log plantations</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"25%"} -->
-<div class="wp-block-column" style="flex-basis:25%"><!-- wp:navigation {"ref":49,"overlayMenu":"never","layout":{"type":"flex","justifyContent":"center","orientation":"vertical","flexWrap":"wrap"},"fontSize":"medium"} /--></div>
+<!-- wp:column {"verticalAlignment":"center","width":"60%","className":"aslnav"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%">
+    <!-- wp:list -->
+    <?php
+    wp_nav_menu(array(
+        'theme_location' => 'footer-menu',
+        'container' => false,
+        'walker' => new Custom_ASL_Nav_Menu()
+    ));
+    ?>
+    <!-- /wp:list -->
+</div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:image {"align":"center","id":69,"width":"120px","sizeSlug":"medium","linkDestination":"none"} -->
+<!-- wp:column {"verticalAlignment":"center","width":"20%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20%"><!-- wp:image {"align":"center","id":69,"width":"120px","sizeSlug":"medium","linkDestination":"none"} -->
 <figure class="wp-block-image aligncenter size-medium is-resized"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/asl-logo-white.png" alt="asl logo white" class="wp-image-69" style="width:120px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
@@ -32,6 +42,6 @@
 <!-- /wp:separator -->
 
 <!-- wp:paragraph -->
-<p>©  <?php echo date('Y') ?> | Aspiring Log Homes</p>
+<p>© <?php echo date('Y') ?> | Aspiring Log Homes</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
