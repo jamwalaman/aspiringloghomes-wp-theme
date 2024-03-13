5fa4f271690d6b79baa60c587f4fdee84493b0b6
Aman Jamwal <aman.jamwal95@gmail.com>
2024-03-14 12:20:00 +1300

    Update header design, header position fixed

--- patterns/header.php
+++ patterns/header.php
@@ -6,14 +6,19 @@
  */
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"20px"}}},"backgroundColor":"base","className":"alh-header","layout":{"type":"constrained"}} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"10px","bottom":"10px"}}},"backgroundColor":"base","className":"alh-header","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide alh-header has-base-background-color has-background"
-	style="padding-top:20px">
+	style="padding-top:10px;padding-bottom:10px">
 	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
 	<div class="wp-block-group alignwide">
 		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex"}} -->
 		<div class="wp-block-group">
-			<!-- wp:site-logo {"width":150 } /-->
+			<!-- wp:image {"id":24,"width":"90px","sizeSlug":"medium","linkDestination":"custom", "className":"logo-main"} -->
+			<figure class="wp-block-image size-medium is-resized logo-main"><a href="<?php echo esc_url(home_url()) ?>"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/logo-alh.svg" alt="alh-logo" class="wp-image-24" style="width:90px"/></a></figure>
+			<!-- /wp:image -->
+			<!-- wp:image {"id":24,"width":"90px","sizeSlug":"medium","linkDestination":"custom", "className":"logo-white"} -->
+			<figure class="wp-block-image size-medium is-resized logo-white"><a href="<?php echo esc_url(home_url()) ?>"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/logo-alh-white.svg" alt="alh-logo-white" class="wp-image-24" style="width:90px"/></a></figure>
+			<!-- /wp:image -->
 		</div>
 		<!-- /wp:group -->
 
