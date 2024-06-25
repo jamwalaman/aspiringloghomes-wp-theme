22db77b26445378115398c6a0cf7bc6dc947aa4a
Aman Jamwal <aman.jamwal95@gmail.com>
2024-06-25 14:20:56 +1200

    Add new pattern block for home page

---
+++ patterns/alh-home-info.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Title: Home section extra info
+ * slug: alh/home-section
+ * Categories: banner
+ */
+?>
+
+<!-- wp:group {"align":"wide","className":"home-info-section-alh","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide home-info-section-alh"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":680,"sizeSlug":"medium","linkDestination":"none"} -->
+<figure class="wp-block-image size-medium"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/home-clock.svg" alt="" class="wp-image-680"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><strong>Mon-Fri 7am-5pm<br>Closed public holidays</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":682,"sizeSlug":"medium","linkDestination":"none"} -->
+<figure class="wp-block-image size-medium"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/home-location.svg" alt="" class="wp-image-682"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><strong>Lot 1 Mcleods Rd<br>Kaikorai Valley Road<br>Dunedin 9012</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":668,"sizeSlug":"medium","linkDestination":"none"} -->
+<figure class="wp-block-image size-medium"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/home-contact.svg" alt="" class="wp-image-668"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><strong>Phone: 021 029 64763<br>Email: <a href="mailto:mat.rusher@aspiringloghomes.co.nz">mat.rusher@aspiringloghomes.co.nz</a></strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
